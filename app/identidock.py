0b8655027c879b4835a09a85736a31257fb55a0b
Noel <anufreos@gmail.com>
2018-11-27 10:18:11 +0000

    add tests and fix xss

--- app/identidock.py
+++ app/identidock.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, request
 import requests
 import hashlib
 import redis
+import html
 
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
@@ -13,7 +14,7 @@ def main_page():
 
     name = default_name
     if request.method == 'POST':
-        name = request.form['name']
+        name = html.escape(request.form['name'], quote=True)
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
     header = '<html><head><title>Identidock</title></head><body>'
@@ -31,6 +32,8 @@ def main_page():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
+
+    name = html.escape(name, quote=True) 
     image = cache.get(name)
     if image is None:
         r =  requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
